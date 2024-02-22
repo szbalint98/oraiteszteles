a77747ffe0e6a0af3e37dd1ab45cdc45002500f6
szbalint98 <ballint3@gmail.com>
2024-02-22 15:32:27 +0100

    .

--- fuggveny.js
+++ fuggveny.js
@@ -1,6 +1,11 @@
 export function parose(szam){
     let psE=false;
-    if (szam%2==0 && szam%1==0) {
+    if (!szam.isNan(szam)) {
+        
+    return"ez nem szam"
+    
+    }
+    if (szam.isInteger(szam)) {
         psE=true;
     }
     return psE;
